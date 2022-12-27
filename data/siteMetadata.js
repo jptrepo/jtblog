7f8f88876a596f12b1846918d81092cd70a72ab9
Joshua Thomas <jpthoma7@outlook.com>
2022-12-27 11:14:33 -0500

    updated site metadata and footer

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,22 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: "Josh's web blog",
+  author: 'Joshua Thomas',
+  headerTitle: 'CloudJPT-3',
+  description:
+    'A blog exploring higher education IT cloud and data topics, projects, musings and more.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/jptrepo/jtblog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
+  github: 'https://github.com/jptrepo',
+  twitter: 'https://twitter.com/jpthoma7',
+  // facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/josh-thomas/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
