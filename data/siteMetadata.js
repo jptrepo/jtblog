ecabe8c2f9c65eae367fca36354d3419d73c73a9
Joshua Thomas <jpthoma7@outlook.com>
2022-12-29 10:46:35 -0500

    changed site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: "Josh's web blog",
   author: 'Joshua Thomas',
-  headerTitle: 'CloudJPT-3',
+  headerTitle: "Joshin' me",
   description:
     'A blog exploring higher education IT cloud and data topics, projects, musings and more.',
   language: 'en-us',
